3f1342621b86fff885dc85e3024911b795fad514
Patrice Alleyne <muniner1@gmail.com>
2017-12-05 23:54:01 -0500

    controllers setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :appointments
-  devise_for :providers
-  devise_for :associates
+  
+  devise_for :providers, controllers: {
+    sessions: 'provider/sessions'
+  }
+  
+  devise_for :associates, controllers: {
+    sessions: 'associates/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
