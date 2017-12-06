419635710da48938a9f9ddcc9a7c3859d96a1e28
Patrice Alleyne <muniner1@gmail.com>
2017-12-05 23:08:28 -0500

    Model setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :appointments
+  devise_for :providers
+  devise_for :associates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
